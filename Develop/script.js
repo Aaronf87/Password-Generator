4df4f8766291ec79490d25fa412a7a54677ca714
Aaron Flores <aaron_flores08@yahoo.com>
2023-06-16 19:26:00 -0500

    added comfirm and comments of to do

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var string = "";
+
+// .length, .random, .floor, for loop, array of numbers,characters,symbols, 
+
+function generatePassword(){
+var char =  prompt("number of characters?");
+
+var num = confirm("Do you want to use numbers?")
+ string = "test"
+console.log("num", num);
+  return string;  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
